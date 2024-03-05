484c78adbbd90d362fb0bc75dc9abbfbb6327567
Nikhil Reddy <nikhildyaga123@gmail.com>
2024-03-05 22:43:20 +0530

    just editing

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,7 @@ const Body = () => {
         <img
           src="https://img.freepik.com/free-vector/recruiting-professionals-studying-candidate-profiles_1262-21404.jpg?w=900&t=st=1709465615~exp=1709466215~hmac=42e471aa2ccc2f93994efaf36e93c2051a7fd0a5193d3b6692332b5ccbf0a794"
           className="w-[55%]"
-          alt="this is homepage logo"
+          alt="this is homepage log"
         />
       </div>
     </div>
